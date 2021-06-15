c7f3d5faf2fe4d423b983cf71d990f9fd0700b86
Conduent <wasim.bari@conduent.com>
2021-06-15 23:34:03 +0530

    added code

--- UI/DB.js
+++ UI/DB.js
@@ -1,28 +1,83 @@
 const MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost/EmployeeDB';
+const client = new MongoClient(url);
 
 
 
-// const insertDocuments = function(db, callback) {
-//     // Get the documents collection
-//     const collection = db.collection('documents');
-//     // Insert some documents
-//     collection.insertMany([{ a: 1 }, { a: 2 }, { a: 3 }], function(err, result) {
-//       assert.equal(err, null);
-//       assert.equal(3, result.result.n);
-//       assert.equal(3, result.ops.length);
-//       console.log('Inserted 3 documents into the collection');
-//       callback(result);
-//     });
-//   };
+async function connectDB() {
+   
+    try {
+        await client.connect();
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }  
+}
 
+async function insertLocationDetail(_product_id, zipcode, currentAddr, locType) {
 
-async function fetchAllItem(prodID) {
+    await connectDB();
+    var locRecord = { productID: _product_id, zip: zipcode, address: currentAddr, locationType: locType };
 
+    if(checkCollectionExists("ItemTracker")) {
+
+        db.collection(_product_id).insertOne(locRecord, function(err, res) {
+            if (err) return err;
+            else return "Record inserted";
+            db.close();
+          });
+
+    } else {
+        client.createCollection("ItemTracker", function(err, res) {
+            if (err) {
+                return  err;
+            } 
+            else{
+                const db = client.db('SupplyChainTracker');
+                db.collection(_product_id).insertOne(locRecord, function(err, res) {
+                    if (err) return err;
+                    else return "Record inserted";
+                    db.close();
+                  });
+            }
+            
+        });
+    }
 
 }
 
+async function checkCollectionExists(_collName) {
+    
+    await connectDB();
+    const db = client.db('SupplyChainTracker');
+    const exists = (await (await db.listCollections().toArray()).findIndex((item) => item.name === _collName) !== -1)
+    return exists;
+
+}
+
+async function fetchAllEvents(_product_id) {
+
+    const db = client.db('SupplyChainTracker');
+    const _itemTracker = b.collection('ItemTracker').find({productID: _product_id});
+    
+    cursor.each(function(err, doc) { 
+        return doc;
+
+    });
+}
 
 async function fetchRecentEvent(prodID) {
+    
+    const db = client.db('SupplyChainTracker');
 
+    db.collection('ItemTracker').findOne(
+        {productID: _product_id},
+        { sort: { _id: -1 } },
+        (err, data) => {
+           console.log(data);
+           return data;
+        },
+      );
 
 }
--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -1,19 +1,23 @@
 pragma solidity 0.5.16;
+pragma experimental ABIEncoderV2;
 
-contract supply_Chain {
+contract SupplyChain {
    
     uint public _p_id =0;
     uint public _u_id =0;
     uint public _t_id=0;
 
+    address _owner;
      struct product {
-       
+
         string _product_name;
-        uint _product_cost;
+        uint   _product_cost;
         string _product_specs;
-        string _product_review;
-        address _product_owner;
-        uint _manufacture_date;
+        string _product_manufacturer;
+        uint256 _product_date;
+        string _product_zipcode;
+        string _product_address;
+        string _product_location;
       
     }
 
@@ -23,7 +27,7 @@ contract supply_Chain {
          string zipcode;
          string currentAddr;
          string locType;// wareshouse, seller, transit
-         string coordinates; //gps coordinates
+       
 
     }
 
@@ -38,122 +42,107 @@ contract supply_Chain {
     }
 
 
+    event AddProduct(uint _product_id, string zipcode, string currentAddr, string locType);
+
     mapping(uint => address) prodToOwnerMapping; //prod to owner mapping
 
-    mapping(uint => track_product) public tracks;
+    mapping(uint => Location[]) tracks;
 
-    mapping(uint => Location) track_product; //product current location..
+    mapping(uint => Location) currentLocation; //product current location..
 
     mapping(uint => product) public products; // prod id and product mapping.
 
-    modifier onlyOwner(uint pid) {
-        require(msg.sender != products[pid]._product_owner);
-         _;
-     }
-    
-    function createParticipant(string memory name ,string memory pass ,address u_add ,string memory utype) public returns (uint){
-        // uint user_id = _u_id++;
-        // participants[user_id]._userName = name ;
-        // participants[user_id]._passWord = pass;
-        // participants[user_id]._address = u_add;
-        // participants[user_id]._userType = utype;
-        
-        return 0;//user_id;
+    mapping(address => uint256[]) userToProdIDs;
+
+    constructor() public {
+       
+        _owner = msg.sender;
+
+        Location memory loc1 = Location(121, "_zipcode1", "_currentAddr1", "_locType1");
+        Location memory loc2 = Location(122, "_zipcode2", "_currentAddr2", "_locType2");
+
+        tracks[101].push(loc1);
+        tracks[101].push(loc2);
+
+        currentLocation[101] = loc1;
+
     }
-    
-    function newProduct(string memory name ,uint p_cost ,string memory p_specs ,string memory p_review) public returns (uint) {
+
+    function newProduct(string memory name ,uint p_cost ,string memory p_specs ,string memory manufacturer, string memory _zipcode, string memory _addr,
+                        string memory _locType) public returns (uint) {
    
             uint product_id = _p_id++;
            
-            
             products[product_id]._product_name = name;
             products[product_id]._product_cost = p_cost;
             products[product_id]._product_specs =p_specs;
-            products[product_id]._product_review =p_review;
-            products[product_id]._product_owner = participants[own_id]._address;
-            products[product_id]._manufacture_date = now;
-            
-           
-            
+            products[product_id]._product_manufacturer =manufacturer;
+            products[product_id]._product_date = now;
+            products[product_id]._product_zipcode =_zipcode;
+            products[product_id]._product_address =_addr;
+            products[product_id]._product_location = _locType; //assuming product owner enter the product.
+
+            prodToOwnerMapping[product_id] == msg.sender;
+
+            Location memory currentLoc = Location(product_id, _zipcode, _addr, _locType);
+            currentLocation[product_id] = currentLoc;
+            tracks[product_id].push(currentLoc);
+
+            emit AddProduct(product_id, _zipcode,  _addr,  _locType);
+
+            userToProdIDs[msg.sender].push(product_id);
+
             return product_id;
 
     }
 
-    function updateProduct(uint p_id, string memory name ,uint p_cost ,string memory p_specs ,string memory p_review) public returns (uint) {
+    
+    function getProduct_details(uint prod_id) public view returns (product memory) {
+     
+            require(prodToOwnerMapping[prod_id] == msg.sender);
+            require(_owner == msg.sender);
+            
+            return products[prod_id];
 
-            require(prodToOwnerMapping[p_id] == msg.sender);
-
-            products[p_id]._product_name = name;
-            products[p_id]._product_cost = p_cost;
-            products[p_id]._product_specs =p_specs;
-            products[p_id]._product_review =p_review;
-
-            return p_id;
     }
 
-    //function getParticipant(uint p_id) returns (string memory ,address,string memory ) public {
-      //  return (participants[p_id]._userName,participants[p_id]._address,participants[p_id]._userType);
-   // }
 
-
-        string _product_name;
-        uint _product_cost;
-        string _product_specs;
-        string _product_review;
-        address _product_owner;
-        uint _manufacture_date;
-
-    function getProduct_details(uint prod_id) public returns (string memory, uint, string memory, string memory, address, uint ) 
-             {
-     
-        return (products[prod_id]._product_name,
-                products[prod_id]._product_cost,  
-                products[prod_id]._product_specs,
-                products[prod_id]._product_review,
-                products[prod_id]._product_owner,
-                products[prod_id]._manufacture_date
-                );
+    function getRecentProductLocation(uint _pId) public view returns(uint, string memory, string memory, string memory) {
+            
+           // require(prodToOwnerMapping[_pId] == msg.sender);
+            //require(_owner == msg.sender);
+            return (currentLocation[_pId]._product_id, currentLocation[_pId].zipcode, currentLocation[_pId].currentAddr,  currentLocation[_pId].locType) ;     
     }
     
    
-  function transferOwnership_product(uint user1_id ,uint user2_id, uint prod_id) onlyOwner(prod_id) public returns(bool) {
-         return (false);
+   function getAllProductLocation(uint _pId) public view returns(Location[] memory) {
+
+          //  require(prodToOwnerMapping[_pId] == msg.sender);
+            //require(_owner == msg.sender);
+            return tracks[_pId];
+            //  return (currentLocation[_pId]._product_id, currentLocation[_pId].zipcode, currentLocation[_pId].currentAddr,  currentLocation[_pId].locType) ;     
     }
-   /* function getProduct_track(uint prod_id)  public  returns (track_product[]) {
+
+    function getAllProductIDs() public view returns(uint256[] memory) {
+
+        return  userToProdIDs[msg.sender];
+
+    }
+
+    function updateProductLocation(uint _pId, string memory _zipcode, string memory _currentAddr, string _locType) public returns(uint) {
+
+        Location memory _updatedLoc = Location(_pId, _zipcode, _currentAddr, _locType);
         
-        uint track_len = tracks[prod_id].length;
-       string[] memory trcks = new string[](track_len);
-       for(uint i=0;i<track_len;i++){
-           track_product t = tracks[prod_id][i];
-           
-           trcks.push(t._product_id+""+t._owner_id+""+t._product_owner+""+t._timeStamp);
-       }
-       // track_product tk =tracks[prod_id];
-         return trcks;
-    }*/
-    function getProduct_trackindex(uint trck_id)  public  returns (uint,uint,address,uint) {
-        
-        track_product memory t = tracks[trck_id];
-       
-         return (t._product_id,t._owner_id,t._product_owner,t._timeStamp);
+        tracks[_pId].push(_updatedLoc);
+
+        currentLocation[_pId] = _updatedLoc;
+
+        return _pId;
     }
     
-   /* function getProduct_chainLength(uint prod_id) public returns (uint) {
-        return tracks.length();
-    }*/
-    
-    function userLogin(uint uid ,string memory  uname ,string memory pass ,string memory utype) public returns (bool){
-       
-       /*
-        if(keccak256(participants[uid]._userType) == keccak256(utype)) {
-            if(keccak256(participants[uid]._userName) == keccak256(uname)) {
-                if(keccak256(participants[uid]._passWord)==keccak256(pass)) {
-                    return (true);
-                }
-            }
-        }
-        */
-        
-        return (false);
+    function putPorductID() public returns(uint256) { //temp...
+                _p_id = _p_id + 1;
+                userToProdIDs[msg.sender].push(_p_id);
+                return _p_id;  
     }
 }
